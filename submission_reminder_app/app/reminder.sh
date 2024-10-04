cb3d36a93fa91697d08950a33e724d9b14b5338c
goldensash1 <s.munyankin@alustudent.com>
2024-10-04 15:17:00 +0200

    changed reminder

--- submission_reminder_app/app/reminder.sh
+++ submission_reminder_app/app/reminder.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+# Script that reminds students of upcoming deadlines
+
+source ../config/config.env
+source ../modules/functions.sh
+
+echo "Checking for upcoming deadlines..."
+
+cat ../assets/submission.txt | while read -r line; do
+    echo "Reminder: $line"
+done
